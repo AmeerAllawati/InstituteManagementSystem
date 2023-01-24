6d747dc99662369d7ae558f8daad9fde366e8055
AmeerAllawati <amaab@ucdavis.com>
2023-01-24 09:40:51 +0400

    All files that are related to the school management is added

---
+++ src/main/java/io/SchoolManagementSystem/Department.java
@@ -0,0 +1,23 @@
+package io.SchoolManagementSystem;
+
+import java.util.ArrayList;
+
+public class Department {
+    public ArrayList<Student> involvedStudents = null;
+    public ArrayList<Teacher> involvedTeacher = null;
+    public ArrayList<Staff> involvedStaff = null;
+    public ArrayList<Course> offeredCourses = null;
+    
+    
+    public void addStudent(Student newStudent) {
+        
+    }
+    
+    public void addStaff(Staff newStaff) {
+        
+    }
+    
+    public void addTeacher(Teacher newTeacher) {
+        
+    }
+}
