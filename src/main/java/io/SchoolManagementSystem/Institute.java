e73f2bbf4714f0756f3802f549c9c743deefbe09
AmeerAllawati <amaab@ucdavis.com>
2023-01-24 09:34:48 +0400

    Added the initial institute file

---
+++ src/main/java/io/SchoolManagementSystem/Institute.java
@@ -0,0 +1,12 @@
+package io.SchoolManagementSystem;
+
+import java.util.ArrayList;
+
+public class Institute {
+
+    public ArrayList<Department> departments = new ArrayList<Department>();
+    
+    public void addDepartment(Department newDepartment) {
+        
+    }
+}
